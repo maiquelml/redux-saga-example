7a54cc4066818b402af5ce1cd4cc18129ab8c9cb
Maiquel Melo <maiquel.melo@compasso.com.br>
2019-10-18 11:31:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# redux-saga-example
+
+### Host
+http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: './app.js'
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './public'
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      exclude: '/node_modules/'
+    }]
+  }
+}
